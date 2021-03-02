c2b09f92f3bf3ed6f1a516e1b94e5e9709bb6ad6
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-02 17:59:23 +0900

    devise root customize

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  devise_for :users
-  root 'static_pages#home'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
 end
