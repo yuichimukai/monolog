164b0bf7a6120b34899857c60c92a8bbea509bfb
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-06 18:43:07 +0900

    login ui cleaned

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
-Rails
-  .application
-  .routes
-  .draw do
+Rails.application.routes.draw do
     root 'static_pages#home'
     get 'static_pages/home'
     get 'static_pages/help'
     devise_for :users,
-               controllers: {
-                 registrations: 'users/registrations',
-                 sessions: 'users/sessions'
-               }
+                controllers: {
+                registrations: 'users/registrations',
+                sessions: 'users/sessions'
+              }
 
     devise_scope :user do
       get 'sign_in', to: 'users/sessions#new'
       get 'sign_out', to: 'users/sessions#destroy'
     end
-  end
+end
