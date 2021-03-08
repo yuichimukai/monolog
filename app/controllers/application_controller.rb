ea688747e6ddd70ae8eb2c224d0b8e8f9ce18ae6
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-08 17:18:56 +0900

    edit login function username or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,19 @@
 class ApplicationController < ActionController::Base
-  # ログイン済ユーザーのみにアクセスを許可する
-  #before_action :authenticate_user!
+	# ログイン済ユーザーのみにアクセスを許可する
+	# before_action :authenticate_user!
 
-  #csrf対策
-  protect_from_forgery with: :exception
+	#csrf対策
+	protect_from_forgery with: :exception
 
-  # deviseコントローラーにストロングパラメータを追加する
-  before_action :configure_permitted_parameters, if: :devise_controller?
+	# deviseコントローラーにストロングパラメータを追加する
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+	protected
 
-  def configure_permitted_parameters
-    added_attrs = [ :email, :username, :password, :password_confirmation ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-  end
-
+	def configure_permitted_parameters
+		added_attrs = %i[email username password password_confirmation]
+		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+		devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,37 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-        :registerable,
-        :recoverable,
-        :rememberable,
-        :validatable
-  validates :username, presence: true
-  validates :profile, length: { maximum: 200 }
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	devise :database_authenticatable,
+	       :registerable,
+	       :recoverable,
+	       :rememberable,
+	       :validatable,
+	       authentication_keys: [:login]
+	validates :username,
+	          presence: true,
+	          uniqueness: {
+			case_sensitive: :false,
+	          },
+	          length: {
+			minimum: 4,
+			maximum: 20,
+	          }
+	validates :profile, length: { maximum: 200 }
+	attr_accessor :login
+
+	def self.find_first_by_auth_conditions(warden_conditions)
+		conditions = warden_conditions.dup
+		if login = conditions.delete(:login)
+			where(conditions)
+				.where(
+					[
+						'username = :value OR lower(email) = lower(:value)',
+						{ value: login },
+					],
+				)
+				.first
+		else
+			where(conditions).first
+		end
+	end
 end
