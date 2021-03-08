245fbe2fc0f585497add07ae25ec5fd2507ff470
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-08 23:24:07 +0900

    fix devise user-edit function by non_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ApplicationRecord
 	validates :profile, length: { maximum: 200 }
 	attr_accessor :login
 
+	#ログイン認証の書き換え(ユーザー名でもメールアドレスでもログインできるようにする)
 	def self.find_first_by_auth_conditions(warden_conditions)
 		conditions = warden_conditions.dup
 		if login = conditions.delete(:login)
@@ -26,12 +27,26 @@ class User < ApplicationRecord
 				.where(
 					[
 						'username = :value OR lower(email) = lower(:value)',
-						{ value: login },
+						{ value: 'メールアドレスまたはユーザ名' },
 					],
 				)
 				.first
 		else
 			where(conditions).first
 		end
+	end
+
+	#devise password入力なしでユーザー情報の更新を行う
+	def update_without_current_password(params, *options)
+		params.delete(:current_password)
+
+		if params[:password].blank? && params[:password_confirmation].blank?
+			params.delete(:password)
+			params.delete(:password_confirmation)
+		end
+
+		result = update_attributes(params, *options)
+		clean_up_passwords
+		result
 	end
 end
