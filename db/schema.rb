6c7318f796bdf3c981f20a071ea5bdfaf1c97582
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-08 15:45:20 +0900

    fix mysql error

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 ActiveRecord::Schema.define(version: 2021_03_07_093957) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username", default: "", null: false
+    t.string "username", limit: 255, default: "", null: false
     t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
