23b736cef7f04a2ce12285f4db5f24fa90e5518d
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-08 18:28:01 +0900

    fix user_edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
-Rails.application.routes.draw do
-  get 'users/show'
-    root 'static_pages#home'
-    get 'static_pages/home'
-    get 'static_pages/help'
-    get 'users/show' => "users#show"
-    devise_for :users,
-                controllers: {
-                registrations: 'users/registrations',
-                sessions: 'users/sessions'
-              }
+Rails
+	.application
+	.routes
+	.draw do
+		get 'users/show'
+		root 'static_pages#home'
+		get 'static_pages/home'
+		get 'static_pages/help'
+		get 'users/show' => 'users#show'
+		get 'users/mypage_edit' => 'users#edit'
+		devise_for :users,
+		           controllers: {
+				registrations: 'users/registrations',
+				sessions: 'users/sessions',
+		           }
 
-    devise_scope :user do
-      get 'sign_in', to: 'users/sessions#new'
-      get 'sign_out', to: 'users/sessions#destroy'
-    end
-end
+		devise_scope :user do
+			get 'sign_in', to: 'users/sessions#new'
+			get 'sign_out', to: 'users/sessions#destroy'
+		end
+	end
