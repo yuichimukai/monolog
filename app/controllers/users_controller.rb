b13a70266c38ce101ddf4f1cee2ea97ff10b7d81
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-15 18:59:08 +0900

    fix header routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,7 @@
 class UsersController < ApplicationController
-	# before_action :set_user, only: %i[show edit update]
-	# def edit
-	# 	redirect_to user_path(@user) unless @user == current_user
-	# end
-	# def update
-	# 	if current_user.update(user_params)
-	# 		redirect_to user_path(current_user)
-	# 	else
-	# 		redirect_to edit_user_path
-	# 	end
-	# end
-	# def user_params
-	# 	params.fetch(:user, {}).permit(:username)
-	# end
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit; end
 end
