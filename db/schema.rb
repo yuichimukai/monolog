13ac939c8ce58bf24c8a8d9b9d306e66348610a3
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-16 22:35:01 +0900

    fix activestorage update-file function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_080052) do
+ActiveRecord::Schema.define(version: 2021_03_15_022432) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2021_03_16_080052) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.text "profile"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
