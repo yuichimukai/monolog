4b301308123446a009a1f20dde7f06c208584bcd
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-21 17:17:10 +0900

    fix follow-follower function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,8 @@ import "bootstrap";
 import "../stylesheets/application";
 import "./bootstrap_custom.js";
 
+// 非同期実装のためにjqueryを読み込ませる
+window.$ = $;
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
