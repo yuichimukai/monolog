1dc259bd08621338ae47be3061424bfe097684c4
yuichimukai <mukai_yuichi@yahoo.co.jp>
2021-03-23 16:46:29 +0900

    add ajax like-button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,14 @@
 class LikesController < ApplicationController
+	before_action :micropost_params
 	def create
 		Like.create(user_id: current_user.id, micropost_id: params[:id])
-		redirect_to microposts_path
 	end
 
 	def destroy
 		Like.find_by(user_id: current_user.id, micropost_id: params[:id]).destroy
-		redirect_to microposts_path
+	end
+
+	def micropost_params
+		@micropost = Micropost.find(params[:id])
 	end
 end
